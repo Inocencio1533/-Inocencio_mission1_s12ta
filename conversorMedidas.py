4b9ac307cd32daf93786ec4d5ce9d674a2657bee
gabrielinocencio <inocenciogabriel69@gmail.com>
2024-06-05 18:16:13 -0300

    Implementação  coverter_cm_para_m()

--- conversorMedidas.py
+++ conversorMedidas.py
@@ -2,8 +2,13 @@ class  ConversorMedidas:
     def __init__self():
         pass
 
-    def converter_cm_para_m():
-        pass
+    def  converter_cm_para_m(valor_cm):
+            valor_metros = valor_cm / 100
+            return valor_metros
 
     def converter_m_para_cm():
         pass
+    
+
+    resultado = ConversorMedidas= converter_cm_para_m(100)
+    print('Resultado:', resultado)
